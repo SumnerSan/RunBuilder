19f23c0b15c90d68822f884662baf7bf74b3e256
SumnerSan <t.stead1@nhs.net>
2019-07-11 14:04:58 +0100

    Adjusted rounding to take place at calculation of above/below/trend

--- functions/stable.R
+++ functions/stable.R
@@ -51,7 +51,7 @@ RunChart = function(measure, subgroup, shiftsens, percentage) {
   base_n = 0
   
   ### Calculate median for first [1:12]
-  dataDF$median = round(median(dataDF$measure[shiftpos:(shiftpos+11)]),0)
+  dataDF$median = median(dataDF$measure[shiftpos:(shiftpos+11)])
   dataDF$baselines = as.numeric(NA) # Create baseline variable but don't calculate until checked for 12 points with no shift
   dataDF$base_n = base_n #Create grouping variable for baselines to disconnect lines
   dataDF$base_label = as.character(NA) #Create labelling variable for baselines
@@ -64,8 +64,8 @@ RunChart = function(measure, subgroup, shiftsens, percentage) {
     dataDF$abovebelow = 0
     dataDF$abovebelow[dataDF$median == 0] = -1   # If the median is zero treat as below
     if (percentage == TRUE) {dataDF$abovebelow[dataDF$median == 100] = 1} #If using percentages and the median is 100, treat as above
-    dataDF$abovebelow[round(dataDF$measure,0) < dataDF$median] = -1
-    dataDF$abovebelow[round(dataDF$measure,0) > dataDF$median] = 1
+    dataDF$abovebelow[round(dataDF$measure,0) < round(dataDF$median,0)] = -1
+    dataDF$abovebelow[round(dataDF$measure,0) > round(dataDF$median,0)] = 1
     
     # Overwrite 12 values at shiftpos with non-useful values if not interrupting baseline
     if(shiftsens == "none") {
@@ -146,7 +146,7 @@ RunChart = function(measure, subgroup, shiftsens, percentage) {
     base_n <- base_n + 1
     dataDF$base_n[shiftpos:length(dataDF$base_n)] = base_n
     # Label baseline
-    dataDF$base_label[shiftpos] = as.character(round(dataDF$median[shiftpos]))
+    dataDF$base_label[shiftpos] = as.character(round(dataDF$median[shiftpos]),0)
     }
     
     # If rebase occurs over 9-11 points at end of dataframe, rebase as temporary
@@ -156,7 +156,7 @@ RunChart = function(measure, subgroup, shiftsens, percentage) {
     base_n <- base_n + 1
     dataDF$base_n[shiftpos:length(dataDF$base_n)] = base_n
     # Label baseline (Temporary)
-    dataDF$base_label[shiftpos] = paste0("Temporary: ", round(dataDF$median[shiftpos]))
+    dataDF$base_label[shiftpos] = paste0("Temporary: ", round(dataDF$median[shiftpos]),0)
     break
     
     }
@@ -167,7 +167,7 @@ RunChart = function(measure, subgroup, shiftsens, percentage) {
       base_n <- base_n + 1
       dataDF$base_n[shiftpos:(newshiftpos - 1)] = base_n
       #Label baseline
-      dataDF$base_label[shiftpos] = paste0("Temporary: ", round(dataDF$median[shiftpos]))
+      dataDF$base_label[shiftpos] = paste0("Temporary: ", round(dataDF$median[shiftpos]),0)
     }
     
     if (newshiftpos == 1) {
@@ -175,7 +175,7 @@ RunChart = function(measure, subgroup, shiftsens, percentage) {
       break} #If newsusshiftpos is 1 there are no more sustained shifts and we can stop looping
     
     ### Recalculate median and extend to end
-    dataDF$median[newshiftpos:length(dataDF$measure)] = round(median(dataDF$measure[newshiftpos:min(newshiftpos+11,(newshiftpos+dataDF$runlength[newshiftpos]-1), nrow(dataDF))]),0)  # Median
+    dataDF$median[newshiftpos:length(dataDF$measure)] = median(dataDF$measure[newshiftpos:min(newshiftpos+11,(newshiftpos+dataDF$runlength[newshiftpos]-1), nrow(dataDF))])  # Median
     dataDF$baselines[newshiftpos:length(dataDF$measure)] = NA
     
     shiftpos = newshiftpos #Make the next sustained shift position the start of the baseline in the next loop
@@ -189,8 +189,8 @@ RunChart = function(measure, subgroup, shiftsens, percentage) {
   dataDF <- dataDF%>%
     dplyr::mutate(trender = case_when(
       row_number() == 1 ~ "none", 
-      measure > lag(measure) ~ "up",
-      measure < lag(measure) ~ "down",
+      round(measure,0) > round(lag(measure),0) ~ "up",
+      round(measure,0) < round(lag(measure),0) ~ "down",
       TRUE ~ "fill"))%>% #Assign trend categories
     mutate(trenderpres = na_if(trender, "fill"))%>% #Coerce values that stay the same to NA for fill function below
     fill(trenderpres, .direction = "down")%>% #Fill periods that stay the same with previous useful observation
